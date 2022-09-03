9f918ae3a7adc29ab9ee869524530356e6375e3b
Keith Fletcher <sourkidk@gmail.com>
2022-09-03 16:54:48 -0400

    finding stops by package ~~~~

--- Truck.py
+++ Truck.py
@@ -40,16 +40,7 @@ class Truck:
         print(self.stops)
 
 
-    def move_to_specific_stop(self, graph, stop):
-        coordinate = (self.current_stop, stop)
-        # print(coordinate)
-        next_stop = stop
-        distance = graph.edge_weights[coordinate]
-        self.miles_driven += distance
-        self.stops.remove(stop)
-        self.current_stop = stop
 
-        self.show_route_specs(distance,next_stop)
 
     def deliver_specific_package(self, package):
         stop_num = None
@@ -79,18 +70,7 @@ class Truck:
         self.show_route_specs(distance, next_stop)
         return
 
-    def move_to_nearest_stop(self, graph):
-        coordinate_list = [(self.current_stop, i) for i in self.stops]
-        # print(coordinate_list)
 
-        distance, next_stop = min((graph.edge_weights[k], k) for k in coordinate_list)
-
-
-        self.miles_driven += distance
-        self.stops.remove(next_stop[1])
-        self.current_stop = next_stop[1]
-
-        self.show_route_specs(distance,next_stop)
 
 
 
