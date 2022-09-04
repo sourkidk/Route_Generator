90f7ffb15187da49fc2c46b2a7ca9a61046b9537
Keith Fletcher <sourkidk@gmail.com>
2022-09-04 15:26:18 -0400

    All functionality completed.

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ from route import *
 from util import *
 
 def main():
+    # displays header infomation
     print_introduction()
 
+    # Instantiates a route object with the appropriate package files and distance table
     today = Route("Today", 'WGUPS Distance Table.csv', 'WGUPS Package File.csv')
+
+    # Most of the program logic exist inside the start_route method
     today.start_route()
 
+    # This while loop runs the user interface.  It prompts the user to enter a time as input to check package states
+    # as well as total mileage driven at that time.  The user can enter 'x' to end the program.
     state = True
-
     while state:
         print('******************************************')
         print('To check the status of all packages at a given time, enter a time in format HH:MM (ie. 13:50 for 1:50 PM)')
@@ -22,6 +27,7 @@ def main():
             state = False
         else:
             try:
+                # Runs in O(n) as it's just iterating through the list and doing a single comparison
                 today.get_status(requested_time)
             except:
                 print('The string you entered is not a valid time.  Please try again.')
--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+# This function iterates through the address and replaces any occurence of an address spelled out with it's single
+# letter abbreviation.
 def harmonize_directions(address)->str:
     directions = ['north', 'east', 'south', 'west', 'North', 'East', 'South', 'West']
     abbrev = ['N', 'E', 'S', 'W']
@@ -9,14 +11,17 @@ def harmonize_directions(address)->str:
             temp = temp
     return temp
 
+# Converts minutes to time format HH:MM
 def minutes_to_time(time_in_minutes):
     return f'{int(time_in_minutes / 60)}:{(int(time_in_minutes) % 60):02}'
 
+# Converts time format HH:MM to minutes
 def time_to_minutes(time: str):
     list = time.split(':')
     total = (int)(list[0]) * 60 + (int)(list[1])
     return total
 
+# displays header infomation
 def print_introduction():
     print('Western Goverornors University - Parcel Service')
     print('-----------------------------------------------')
