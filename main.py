68f86b30f2ad6e0ee12fbfdd9631c10a2cb6bd1b
Keith Fletcher <sourkidk@gmail.com>
2022-09-06 06:08:37 -0400

    Additional UI functionality added.

--- main.py
+++ main.py
@@ -21,17 +21,39 @@ def main():
     state = True
     while state:
         print('******************************************')
-        print('To check the status of all packages at a given time, enter a time in format HH:MM (ie. 13:50 for 1:50 PM)')
-        requested_time = input('To exit, enter \'x\'...\n')
-        if requested_time == 'x':
-            state = False
-            break
-        else:
-            try:
-                # Runs in O(n) as it's just iterating through the list and doing a single comparison
-                today.get_status(requested_time)
-            except:
-                print('The string you entered is not a valid time.  Please try again.')
+        print('Menu:')
+        print('Get the status of all packages >>> Type \'all\'')
+        print('Get a single package status >>> Type \'any\'')
+        menu_selection = input('To exit, enter \'x\'...\n')
+
+        try:
+            if menu_selection.lower() == 'x':
+                state = False
+                break
+
+            elif menu_selection.lower() == 'all':
+                try:
+                    time_entry = input('To check the status of all packages at a given time, enter a time in format HH:MM (ie. 13:50 for 1:50 PM)\n')
+                    # Runs in O(n) as it's just iterating through the list and doing a single comparison
+                    today.get_status(time_entry)
+                except:
+                    print('The string you entered is not a valid time.  Please try again.')
+            elif menu_selection.lower() == 'any':
+                package_entry = int(input('Enter the integer id number for a package to view its status...(i.e. 5 for package 5 )\n'))
+                try:
+                    single_package_time = input('To check the status of this packages at a given time, enter a time in format HH:MM (ie. 13:50 for 1:50 PM)\n')
+                    # Runs in 0(1) as it's effectively a lookup with function call.
+                    today.package_hash.lookup(package_entry).get_status(single_package_time)
+                except:
+                    print('The string you entered is not a valid time.  Please try again.')
+            else:
+                print("Try Again")
+
+
+
+
+        except:
+            print('The string you entered is not a valid selection.  Please try again.')
 
     print("Exiting WGU-PS...")
 
